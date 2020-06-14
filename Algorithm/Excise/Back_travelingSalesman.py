f63e20a5012448e8563c6bdb8c40697f80ea4239
Harvey <hw-li@foxmail.com>
2020-06-14 09:52:33 +0800

    Add traveling salesman

--- Algorithm/Excise/Back_travelingSalesman.py
+++ Algorithm/Excise/Back_travelingSalesman.py
@@ -7,13 +7,37 @@
 '''
 
 
-def Traveling(n: int, E: list) -> list:
-    x = [0]*n
-    xo = x[:]
-    i = 0
-    while i < n:
+def Traveling(n: int, E: list) -> [int, list]:
+    treelist = generateTree(n)
+    min_road = []
+    B = 0
+    for i in range(len(treelist)):
+        for j in range(n):
+            F = cost_func(treelist[i], j, E)
+            if F > B and B > 0:
+                break
+            if (j == n-1 and F < B) or (j == n-1 and i == 0):
+                B = F
+                min_road = treelist[i][:]
+    return B, min_road
 
-        i = i+1
+
+def cost_func(t, j, E):
+    L = 0
+    for i in range(j):
+        L = L + E[t[i]][t[i+1]]
+    if j < len(t)-1:
+        L = L + min_dist(t[j], E[t[j]]) + total_min_dist(t[j+1:len(t)], E)
+    else:
+        L = L+E[t[j]][t[0]]
+    return L
+
+
+def total_min_dist(a, E):
+    L = 0
+    for i in a:
+        L = L + min_dist(i, E[i])
+    return L
 
 
 def min_dist(i, d):
@@ -22,24 +46,16 @@ def min_dist(i, d):
     return min(temp)
 
 
-def factorial(n):
-    if(n > 1):
-        z = n*factorial(n-1)
-        return z
-    else:
-        return 1
-
-
 def generateTree(n):
     treelist = []
-    city = [i+1 for i in range(n)]
+    city = [i for i in range(n)]
     citysign = [0]*n
     y = [0]*n
     i = 0
     while i < n:
         y[i] = next_value(y[0:i], city)
         citysign[i] = 1
-        if i == 3:
+        if i == n-1:
             # print(y)
             treelist.append(y[:])
             [y, i] = tree_back(y, i, city)
@@ -53,7 +69,7 @@ def tree_back(y, i, city):
     i = i-1
     if i == 0:
         return y, i
-    if y[i]+1 <= 4:
+    if y[i]+1 <= len(city)-1:
         y[i] = y[i]+1
         if y[i] in y[0:i]:
             [y, i] = tree_back(y, i+1, city)
@@ -82,8 +98,7 @@ if __name__ == '__main__':
          [9, 13, 0, 7],
          [4, 2, 7, 0]]
     n = 4
-    # Traveling(4,E)
-    # a = factorial(3)
-    # print(a)
-    treelist = generateTree(4)
-    print(treelist)
+    [min_length, min_road]=Traveling(4, E)
+    min_road = [i+1 for i in min_road]
+    print('minimum traveling road: '+str(min_road))
+    print('minimum length is '+ str(min_length) + ' km')
