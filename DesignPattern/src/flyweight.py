c9ac1547ab873b3f789834329b3505ebc0504edd
Harvey1024 <hw-li@foxmail.com>
2020-07-12 11:42:25 +0800

    Add flyweight.py

--- DesignPattern/src/flyweight.py
+++ DesignPattern/src/flyweight.py
@@ -1,20 +1,44 @@
+# Flyweight Patterns
 
-class FlyweightFactory:
-  def getFlyweight():
-    if(itr == m_myFlyweight.end()):
-      fw = ConcreteFlyweight(str)
-    else:
-      
+
 class Flyweight:
-  def __init__(self):
-    self._intrinsicState = ''
-  def GetS
-  def operation:
-    pass
+    def __init__(self):
+        self._intrinsicState = ''
+
+    def GetState(self):
+        return self._intrinsicState
+
+    def operation(self):
+        pass
+
 
 class ConcreteFlyweight(Flyweight):
-  def __init__(self):
-    self._intrinsicState = None
-  def operation():
-    pass
+    # instances = []
+    def __init__(self, state):
+        self._intrinsicState = state
+        # self.__class__.instances.append(self)
 
+    def operation(self):
+        print("ConcreteFlyweight: %s" % self.GetState())
+
+
+class FlyweightFactory:
+    def __init__(self):
+        self.flys = []
+
+    def getFlyweight(self, key: str):
+        for obj in self.flys:
+            if(obj._intrinsicState == key):
+                print("already created by users...")
+                return obj
+        print("creat new concreteFlyweight: %s" % key)
+        fn = ConcreteFlyweight(key)
+        self.flys.append(fn)
+        return fn
+
+
+if __name__ == "__main__":
+    fc = FlyweightFactory()
+    fw1 = fc.getFlyweight("hello")
+    fw2 = fc.getFlyweight("world")
+    fw3 = fc.getFlyweight("hello")
