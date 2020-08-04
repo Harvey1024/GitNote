1565414eba54fd36a482228709fcd718c530e93e
Harvey1024 <hw-li@foxmail.com>
2020-08-04 19:17:14 +0800

    Add chain pattern

--- DesignPattern/src/chain.py
+++ DesignPattern/src/chain.py
@@ -1,1 +1,27 @@
 # Chain of responsibility
+# Chain of responsibility
+class RequestType:
+    def __init__(self):
+        self.COLLECT_TAX = None
+        self.DEFEND_CASTLE = None
+        SELF.TORTURE_PRISONER = None
+    def valueOf(self, name: str):
+        return self
+    def values(self):
+        pass
+
+class Request:
+    def __init__(self):
+        pass
+
+
+class RequestHander:
+    def __init__(self):
+        self.Logger = None
+        self.next = None
+
+    def handleRequest(self, req: Request):
+        pass
+
+    def printHandling(self, req: Request):
+        pass
